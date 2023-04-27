498ced3952662d9771cf50bb719984611e05357f
abmakko <36611833+abmakko@users.noreply.github.com>
2023-04-27 11:56:51 +0300

    Add ImagePREVIEW to Home, Use Js to Switch IMAGES
    Use div containers to set background image preview, added a new Js file to project
    which handles the image switching based on mouse cursor pointers

---
+++ script.js
@@ -0,0 +1,52 @@
+const img_1 = document.getElementById('glazed_chicken');
+const button_1 = document.getElementById('b-one');
+
+const default_img = document.getElementById('default_image');
+
+const button_2 = document.getElementById('b-two');
+const img_2 = document.getElementById('rice_chicken');
+
+const button_3 = document.getElementById('b-three');
+const img_3 = document.getElementById('pork');
+
+button_1.addEventListener('mouseover', function handleMouseOver(){
+    img_1.style.display = 'block';
+    default_img.style.display = 'none';    
+    img_2.style.display = 'none';
+    img_3.style.display = 'none';
+});
+
+button_1.addEventListener('mouseout', function handleMouseOut(){
+    default_img.style.display = 'block';
+    img_1.style.display = 'none';
+    img_2.style.display = 'none';
+    img_3.style.display = 'none';
+});
+
+button_2.addEventListener('mouseover', function handleMouseOver(){
+    img_2.style.display = 'block';
+    default_img.style.display = 'none';
+    img_1.style.display = 'none';    
+    img_3.style.display = 'none';
+});    
+
+button_2.addEventListener('mouseout', function handleMouseOut(){
+    default_img.style.display = 'block';
+    img_1.style.display = 'none';
+    img_2.style.display = 'none';
+    img_3.style.display = 'none';
+});
+
+button_3.addEventListener('mouseover', function handleMouseOut(){
+    img_3.style.display = 'block';
+    img_1.style.display = 'none';
+    img_2.style.display = 'none';
+    default_img.style.display = 'none';    
+});
+
+button_3.addEventListener('mouseout', function handleMouseOut(){
+    default_img.style.display = 'block';
+    img_1.style.display = 'none';
+    img_2.style.display = 'none';
+    img_3.style.display = 'none';
+});
