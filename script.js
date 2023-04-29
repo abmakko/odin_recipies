326a8b3907bea4080ba17f05846eec93faf54526
abmakko <36611833+abmakko@users.noreply.github.com>
2023-04-29 14:56:18 +0300

    added page loader to landing page

--- script.js
+++ script.js
@@ -66,3 +66,17 @@ right_arrow.addEventListener('click', function handleClick(){
     
 })
 
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+        "body").style.visibility = "hidden";
+        document.querySelector(
+        "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+        "#loader").style.display = "none";
+        document.querySelector(
+        "body").style.visibility = "visible";
+    }
+};
+
